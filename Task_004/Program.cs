18e272c288cb3adfe2bbf1419213071647590344
htcak <htcakuma@gmail.com>
2020-12-14 02:41:02 +0200

    Complete 4th task

---
+++ Task_004/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Task_004
+{
+    class Program
+    {
+        static void FindCLients(out int clients) 
+        {
+            while (true)
+            {
+                Console.WriteLine("Eneter amount of clients");
+                if (!int.TryParse(Console.ReadLine(), out clients))
+                {
+                    Console.WriteLine("Wrong input...");
+                }
+                else break;
+            }
+        }
+
+        static int FindAllRoutes(int clientsAmount) 
+        {
+            int counter = 1;
+            int result = 1;
+
+            if (clientsAmount == 0) 
+            {
+                return result;
+            }
+
+            do
+            {
+                result *= counter;
+                counter++;
+                
+
+            } while (counter <= clientsAmount);
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int clientsAmount;
+            FindCLients(out clientsAmount);
+            Console.WriteLine(FindAllRoutes(clientsAmount).ToString());
+
+            //Delay 
+            Console.ReadKey();
+        }
+    }
+}
