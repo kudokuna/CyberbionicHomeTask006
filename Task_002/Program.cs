9f8c3a61b1348aef0d7e9b50164d23dc0a868cc1
htcak <htcakuma@gmail.com>
2020-12-14 02:19:22 +0200

    Refactor Task 2 and Task 3 Remove old Project

---
+++ Task_002/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+
+namespace Task_002
+{
+    class Program
+    {
+        static void SetValues(out int firstValue, out int secondValue)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Enter First Value");
+                string userInputFirst = Console.ReadLine();
+                Console.WriteLine("Enter Second Value");
+                string userInputSecond = Console.ReadLine();
+                if ((!int.TryParse(userInputFirst, out firstValue)) || (!int.TryParse(userInputSecond, out secondValue)) || (secondValue < firstValue))
+                {
+                    Console.WriteLine("Wrong Input. You typed incorrect numbers or second value is smaller than first value...");
+                    Console.WriteLine("Hit Enter to type again");
+                    Console.ReadKey();
+                }
+                else break;
+            }
+        }
+
+        public static void ShowSumAndOddValues(int firstValue, int secondValue)
+        {
+            int summOfValues = default;
+
+            for (int i = firstValue + 1; i < secondValue; i++)
+            {
+                summOfValues += i;
+            }
+
+            Console.WriteLine($"Summ of values is {summOfValues}");
+            Console.WriteLine($"odd values between {firstValue} and {secondValue} are ...");
+
+            for (int i = firstValue + 1; i < secondValue; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.ReadKey();
+        }
+
+
+        static void Main(string[] args)
+        {
+            int firstValue, secondValue;
+            SetValues(out firstValue, out secondValue);
+            ShowSumAndOddValues(firstValue, secondValue);
+
+            //delay
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task_003/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Task_003
+{
+    class Program
+    {
+        public static void DrawFigures()
+        {
+            Console.WriteLine("Draw figure 1 -- rectangle");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("\n");
+                for (int j = 0; j < 6; j++)
+                {
+                    Console.Write("*");
+                    Console.Write(" ");
+                }
+            }
+
+            Console.WriteLine("\n Draw figure 2 -- right triangle\n");
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(" ");
+                    Console.Write('*');
+                    Console.Write(" ");
+                }
+                Console.Write("\n");
+            }
+
+            Console.WriteLine("\n Draw figure 3 -- right triangle\n");
+            {
+                int i = 1;
+                while (i <= 5)
+                {
+                    int k = 5;
+                    int h = 1;
+
+                    Console.Write("\n");
+                    while (k > i)
+                    {
+                        Console.Write(" ");
+                        k--;
+                    }
+                    while (h <= i)
+                    {
+                        Console.Write("*");
+                        Console.Write("*");
+                        h++;
+                    }
+                    i++;
+                }
+            }
+        }
+
+            static void Main(string[] args)
+        {
+            DrawFigures();
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
