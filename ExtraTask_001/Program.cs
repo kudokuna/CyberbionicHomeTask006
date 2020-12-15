45160cceb6178743ab6fdf790e76f76b28eeab62
htcak <htcakuma@gmail.com>
2020-12-16 01:56:46 +0200

    Add ExtraTasks

---
+++ ExtraTask_001/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ExtraTask_001
+{
+    class Program
+    {
+        static void DefineRectangleSize(out int height, out int width)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Please Set Rectangle height:");
+                string heightString = Console.ReadLine();
+                Console.WriteLine("Please select rectangle width:");
+                string widthString = Console.ReadLine();
+
+                if (!int.TryParse(heightString, out height) || (!int.TryParse(widthString, out width)) || height <= 0 || width <= 0)
+                {
+                    Console.WriteLine("Wrong input");
+                }
+                else break;
+
+            }
+            Console.Write("\n");
+        }
+
+        static void DrawRectangle(int height, int width)
+        {
+            for (int i = 0; i < height; i++)
+            {
+                Console.Write("*");
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int height, width;
+            DefineRectangleSize(out height, out width);
+            DrawRectangle(height, width);
+
+            //Delay
+            Console.ReadKey();
+        }
+    }
+}
--- Task_005/Program.cs
+++ Task_005/Program.cs
@@ -4,41 +4,105 @@ namespace Task_005
 {
     class Program
     {
-        static void DefineSymbols(out int height, out int width, out string symbol) 
+
+        static void DefineRectangleSize(out int height, out int width, out char symbol)
         {
             while (true)
             {
                 Console.Clear();
-                Console.WriteLine("Enter height Value");
-                string userInputFirst = Console.ReadLine();
-                Console.WriteLine("Enter width Value");
-                string userInputSecond = Console.ReadLine();
-                if ((!int.TryParse(userInputFirst, out height)) || (!int.TryParse(userInputSecond, out width)))
-                {
-                    Console.WriteLine("Wrong Input...");
-                    Console.WriteLine("Hit Enter to type again");
-                    Console.ReadKey();
-                }
-                else break;
-            }
+                Console.WriteLine("Please Set Rectangle height:");
+                string heightString = Console.ReadLine();
+                Console.WriteLine("Please select rectangle width:");
+                string widthString = Console.ReadLine();
 
-            Console.WriteLine("Please define symbol...");
-            symbol = Console.ReadLine();
+                if (!int.TryParse(heightString, out height) || (!int.TryParse(widthString, out width)) || height <= 0 || width <= 0)
+                {
+                    Console.WriteLine("Wrong input");
+                } else break;
+                
+            }
+            
+            Console.WriteLine("Please type symbol for paint rectangle:");
+            symbol = Console.ReadKey().KeyChar;
         }
-        static void DrawFigure(int height, int width, string symbol) 
+
+        static void DrawRectangle(int coordX, int coordY, int height, int width, char symbol)
+        {
+            Console.Clear();
+            for (int i = 0; i < width + coordY; i++)
+            {
+                for (int j = 0; j < height + coordX; j++)
+                {
+                    if (i < coordY || j < coordX)
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write(symbol);
+                    }
+                }
+
+                Console.Write("\n");
+            }
+        }
+
+        static void MoveRectangleViaSpider(int coordinateX, int coordinateY, int height, int width, char symbol) 
         {
 
+            for (; ; )
+            {
+                ConsoleKey keyPressed = Console.ReadKey(true).Key;
+
+                if (keyPressed == ConsoleKey.Escape)
+                {
+                    break;
+                }
+
+                switch (keyPressed)
+                {
+                    case ConsoleKey.D:
+                    case ConsoleKey.RightArrow:
+                        coordinateX += 1;
+                        DrawRectangle(coordinateX, coordinateY, height, width, symbol);
+                        break;
+                    case ConsoleKey.A:
+                    case ConsoleKey.LeftArrow:
+                        coordinateX = coordinateX - 1 < 0 ? 0 : coordinateX - 1;
+                        DrawRectangle(coordinateX, coordinateY, height, width, symbol);
+                        break;
+                    case ConsoleKey.W:
+                    case ConsoleKey.UpArrow:
+                        coordinateY = coordinateY - 1 < 0 ? 0 : coordinateY - 1;
+                        DrawRectangle(coordinateX, coordinateY, height, width, symbol);
+                        break;
+                    case ConsoleKey.S:
+                    case ConsoleKey.DownArrow:
+                        coordinateY += 1;
+                        DrawRectangle(coordinateX, coordinateY, height, width, symbol);
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Input");
+                        break;
+                }
+            }
         }
+
+
 
         static void Main(string[] args)
         {
-            int height, width;
-            string symbol;
-            DefineSymbols(out height, out width, out symbol);
-            DrawFigure(height, width, symbol);
+            int coordinateX = default;
+            int coordinateY = default;
+            int height;
+            int width;
+            char symbol;
 
-            //Delay
-            Console.ReadKey();
+            DefineRectangleSize(out height, out width, out symbol);
+            DrawRectangle(coordinateX, coordinateY, height, width, symbol);
+            MoveRectangleViaSpider(coordinateX, coordinateY, height, width, symbol);
+
         }
+
     }
 }
